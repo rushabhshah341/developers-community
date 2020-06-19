d419ddd14d0bebc0dca43356217fb9c94c2548ba
Andrew Cosmic <acosmic@liveperson.com>
2020-06-18 17:58:28 -0700

    moved new build into register-test

---
+++ register-test/precache-manifest.984f05ddadddaf94d3a99fea32c616a9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89acf178b3186632fa6d0d3f9ffe8850",
+    "url": "/index.html"
+  },
+  {
+    "revision": "224641dc481b57a6522e",
+    "url": "/static/css/2.117ce29f.chunk.css"
+  },
+  {
+    "revision": "95e932071c50920cae95",
+    "url": "/static/css/main.6003f4ba.chunk.css"
+  },
+  {
+    "revision": "224641dc481b57a6522e",
+    "url": "/static/js/2.38534807.chunk.js"
+  },
+  {
+    "revision": "2509ae3ddba6d42fbab98ac19e21fb9b",
+    "url": "/static/js/2.38534807.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95e932071c50920cae95",
+    "url": "/static/js/main.57d4791a.chunk.js"
+  },
+  {
+    "revision": "7d4a52674b194d8da82c",
+    "url": "/static/js/runtime-main.3bbbbf6f.js"
+  },
+  {
+    "revision": "3bf9ba3fdb9d6e92a8694f8da6644c9c",
+    "url": "/static/media/carousel-1.3bf9ba3f.jpg"
+  },
+  {
+    "revision": "65cb6e5fb1486927feee607c8a83a1fd",
+    "url": "/static/media/carousel-2.65cb6e5f.jpg"
+  },
+  {
+    "revision": "d856c9ba77758392e5bbcfeffc004744",
+    "url": "/static/media/carousel-3.d856c9ba.jpg"
+  },
+  {
+    "revision": "03f17e871e611cc53908d5151380e349",
+    "url": "/static/media/close.03f17e87.svg"
+  },
+  {
+    "revision": "ca50d9dd2bc6b6be69b24a2de56c2d2a",
+    "url": "/static/media/dropdown.ca50d9dd.svg"
+  },
+  {
+    "revision": "df9acfdbd9b0eae6bbfe8ddfe3c4f8f1",
+    "url": "/static/media/logo.df9acfdb.svg"
+  }
+]);
