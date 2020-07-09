9937b9b3afd0117fc7ba350a55f75b9b9f0936b7
Christopher Voncina <cvoncina@liveperson.com>
2020-07-09 13:20:55 -0700

    not broken verify

--- _scripts/verify.js
+++ _scripts/verify.js
@@ -1,19 +1,52 @@
 // let reader = new FileReader();
 let solution = require('./solutions.json');
+let solution_path = "pages/Solutions/MMS/additionaloptions.md";
+let document_path = "pages/documents/";
+const fs = require("fs"); // Or `import fs from "fs";` with ESM
+
 (function () {
    console.log(solution[0])
+   // update solution path
+   if (fs.existsSync(solution_path)) {
+      // Do something
+      console.log('the first file exists');
+      //now loop 
+   }
    solution.forEach(item => {
       // TODO use file reader to see if folder exists
-      if (folderName(Nameitem.solutionname) && item.documents) {
+      if (folderName(item.solutionname) && item.documents) {
          item.documents.forEach(doc => {
             // TODO use file reader to see if folder exists
-
+            solution_path = "pages/Solutions/";
             //is this doc a folder 
             if (doc.documentname.pages) {
-               // check if folder exists
+               // check if folder exists 
+               // update solution path
+               if (fs.existsSync(solution_path)) {
+                  // Do something
+                  console.log('folder exists');
+                  //now loop 
+               }
+               else {
+                  console.log(`${doc.documentname} file doesn't exist or is not named properly.
+               file name should take title, make it lower case and add dashes where spaces are
+               i.e.  File Name should be file-name
+               `);
+               }
                //loop
-            } else if (fileName(doc.documentname)) {
+            } else if (doc.documentname) {
                // check if file exists
+               let fName = fileName(doc.documentname);
+               //solution_path += filefName + '.md';
+               console.log('solution path is : ', solution_path);
+               if (fs.existsSync(solution_path)) {
+                  // Do something
+                  console.log('This file exists: ', solution_path);
+                  //now loop 
+               }
+               else {
+                  console.log('Could not find this file ', solution_path);
+               }
             } else {
                console.log(`${doc.documentname} file doesn't exist or is not named properly.
                   file name should take title, make it lower case and add dashes where spaces are
@@ -33,10 +66,12 @@ let solution = require('./solutions.json');
 }());
 
 function fileName(file) {
-   return file.toLowerCase().split(' ').join('-')
+   console.log('file name is:', file.toLowerCase().split(' ').join('-').trim());
+   return file.toLowerCase().split(' ').join('-').trim()
 }
 function folderName(folder) {
-   return folder.split(' ').join('')
+   console.log('Folder name is:', folder.split(' ').join('').trim());
+   return folder.split(' ').join('').trim();
 }
 
 // forEach(item ){
