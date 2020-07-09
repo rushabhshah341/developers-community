3c43a021765b89c8a286abdfcf929773f40d675a
Andrew Cosmic <acosmic@liveperson.com>
2020-07-09 14:18:18 -0700

    Working for solutons. Possibly working for documents

--- _scripts/verify.js
+++ _scripts/verify.js
@@ -5,52 +5,53 @@ let document_path = "pages/documents/";
 const fs = require("fs"); // Or `import fs from "fs";` with ESM
 
 (function () {
-   console.log(solution[0])
-   // update solution path
-   if (fs.existsSync(solution_path)) {
-      // Do something
-      console.log('the first file exists');
-      //now loop 
-   }
+
+   let pass = true;
    solution.forEach(item => {
       // TODO use file reader to see if folder exists
       solution_path = "pages/Solutions/";
       solution_path += folderName(item.solutionname) + '/';
-      console.log('Solution path updated :', solution_path)
+      // console.log('Solution path updated :', solution_path)
       if (fs.existsSync(solution_path) && item.documents) {
-         console.log('Found folder HOOORAH :', folderName(item.solutionname))
+         // console.log('Found folder HOOORAH :', folderName(item.solutionname))
 
          item.documents.forEach(doc => {
             // TODO use file reader to see if folder exists
+            var document_path = solution_path
 
-            //is this doc a folder 
+            //check if document is a folder 
             if (doc.documentname.pages) {
-               // check if folder exists 
-               // update solution path
-               if (fs.existsSync(solution_path)) {
-                  // Do something
-                  console.log('folder exists');
-                  //now loop 
+               //TODO add for loop for pages.check
+               // console.log("Doc is a folder")
+               document_path += folderName(doc.documentname) + '/'
+               if (fs.existsSync(document_path)) {
+                  // Check if pages have subpages
+                  console.log('Doc Folder exists');
+                  //now loop over pages
                }
                else {
-                  console.log(`${doc.documentname} file doesn't exist or is not named properly.
-               file name should take title, make it lower case and add dashes where spaces are
-               i.e.  File Name should be file-name
-               `);
+                  console.log(`${doc.documentname} file doesn't exist or is not named properly.`);
                }
-               //loop
-            } else if (doc.documentname) {
+            }
+            //check if document is a file
+            else if (doc.documentname) {
                // check if file exists
                let fName = fileName(doc.documentname);
+               document_path = solution_path + fName + '.md'
                //solution_path += filefName + '.md';
-               console.log('solution path is : ', solution_path);
-               if (fs.existsSync(solution_path)) {
-                  // Do something
-                  console.log('This file exists: ', solution_path);
-                  //now loop 
+               // console.log('solution path is : ', solution_path);
+               if (fs.existsSync(document_path)) {
+                  console.log('This file exists: ', document_path);
+                  let pagename = get_line(document_path, 1, function (err, line) {
+                     // console.log("pagename&&&:", line.substring(10));
+                     console.log("\n");
+                     if (!(doc.documentname === line.substring(10))) {
+                        console.log("FILENAME IS BAD!")
+                     }
+                  })
                }
                else {
-                  console.log('Could not find this file ', solution_path);
+                  console.log('Could not find this file ', document_path);
                }
             } else {
                console.log(`${doc.documentname} file doesn't exist or is not named properly.
@@ -69,52 +70,22 @@ const fs = require("fs"); // Or `import fs from "fs";` with ESM
 }());
 
 function fileName(file) {
-   console.log('file name is:', file.toLowerCase().split(' ').join('-').trim());
+   // console.log('file name is:', file.toLowerCase().split(' ').join('-').trim());
    return file.toLowerCase().split(' ').join('-').trim()
 }
 function folderName(folder) {
-   console.log('Folder name is:', folder.split(' ').join('').trim());
+   // console.log('Folder name is:', folder.split(' ').join('').trim());
    return folder.split(' ').join('').trim();
 }
 
-// forEach(item ){
-//     if (item.solutionname.a.split(' ').join('')  && item.documents ){ // check folder exists? -- fileread
-//         item.documents.forEach(doc){
-//         // check if each file name is not correct -- file read
-//             if( !doc.documentname ){
-//                 console.log( `${doc.documentname} file doesn't exist or is not named properly. 
-//                 file name should take title, make it lower case and add dashes where spaces are
-//                 i.e.  File Name should be file-name
-//                 `);
-//             } else{
+function get_line(filename, line_no, callback) {
+   var data = fs.readFileSync(filename, 'utf8');
+   var lines = data.split("\n");
 
+   if (+line_no > lines.length) {
+      throw new Error('File end reached without finding line');
+   }
 
-//                // if(doc.pages){
-//                //    doc.page.forEach(page){
-
-//                //    else {
-//                //       if (page.subpages){
-
-//                //       }
-
-//                //    }
-
-//             } else{
-//                   console.log(`${doc.documentname} file doesn't exist or is not named properly. 
-//                 file name should take title, make it lower case and add dashes where spaces are
-//                 i.e.  File Name should be file-name
-//                 `);
-//                }
-//             }
-//         }
-//     } else{
-//     print `${item.solutionname}  folder doesn't exist or is not named properly. 
-//         folder name should take title and squash it.
-//         i.e.  File Name should be FileName.
-//         `
-//     }   
-
-
-
-// }
+   callback(null, lines[+line_no]);
+}
 
